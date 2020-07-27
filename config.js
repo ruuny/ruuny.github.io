9e4cb9516af39ab5763cfa626d1265cf2dcb87a5
bglgwyng <bglgwyng@gmail.com>
2020-07-27 18:44:32 +0900

    Update configs to mines

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://bglgwyng.github.io/',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Blog by bglgwyng',
+  subtitle: '',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -25,19 +25,19 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: 'bglgwyng',
+    photo: '',
+    bio: '',
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
+      email: 'bglgwyng@gmail.com',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: 'bglgwyng',
       rss: '',
       vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
+      linkedin: '',
+      instagram: '',
       line: '',
       gitlab: '',
       weibo: '',
---
+++ content/posts/2020-07-27---Hello-World!.md
@@ -0,0 +1,14 @@
+---
+title: Hello World!
+date: "2020-07-27T09:37:25.471Z"
+template: "post"
+draft: false
+slug: "hello-world"
+category: "Test"
+tags:
+  - "Test"
+description: "A greeting to the world"
+socialImage: "/media/42-line-bible.jpg"
+---
+
+Hello World!
